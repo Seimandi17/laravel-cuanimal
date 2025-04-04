43370bb19a1dd19a7e5a6fe0138ea1c16990fef5
Valentín <vale.seimandi@hotmail.com>
2025-04-04 09:31:50 -0300

    nueva actua

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://cuanimal.com'),
+    'url' => env('APP_URL', 'http://localhost'),
 
     /*
     |--------------------------------------------------------------------------
